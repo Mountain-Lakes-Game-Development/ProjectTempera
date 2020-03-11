d83cd666f926fd38c07b68e46c2a85cdac4d141b
Rithsagea <rithsagea@gmail.com>
2020-03-10 20:38:55 -0400

    Documentation

--- ProjectTempera/src/api/tempera/math/Segment.java
+++ ProjectTempera/src/api/tempera/math/Segment.java
@@ -5,6 +5,11 @@ public class Segment {
 	protected Vector pointA;
 	protected Vector pointB;
 	
+	/**
+	 * Creates a line segment from two points
+	 * @param pointA a vector representing a point
+	 * @param pointB a vector representing a point
+	 */
 	public Segment(Vector pointA, Vector pointB) {
 		this.pointA = pointA;
 		this.pointB = pointB;
