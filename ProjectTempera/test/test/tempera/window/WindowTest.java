084ab472be0d0b9c304039dcc78b14b0ffb3a689
Rithsagea <rithsagea@gmail.com>
2020-03-28 01:23:08 -0400

    Added some more tests

--- ProjectTempera/test/test/tempera/window/WindowTest.java
+++ ProjectTempera/test/test/tempera/window/WindowTest.java
@@ -1,5 +1,6 @@
 package test.tempera.window;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -15,10 +16,31 @@ public class WindowTest {
 		TemperaWindow window = new TemperaWindow(1000, 500);
 		TemperaPanel panel = window.getPanel();
 		
-		ImageObject image = new ImageObject(ImageIO.read(new File("src/resources/flamingo.jpg")));
+		BufferedImage plus = ImageIO.read(new File("src/resources/plus.png"));
+		
+		ImageObject image = new ImageObject(plus, 100, 100);
 		Vector pos = image.getPosition();
+		pos.setX(0).setY(0);
 		
 		panel.addObject(image);
 		
+		Vector mouse = window.getMouse();
+		
+		Thread thread = new Thread() {
+			public void run() {
+				while(true) {
+					pos.setX(mouse.getX()).setY(mouse.getY());
+					System.out.println(pos);
+					
+					try {
+						sleep(10);
+					} catch(Exception e) {
+						
+					}
+				}
+			}
+		};
+		
+		thread.run();
 	}
 }
